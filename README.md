a5c436a75347fc7b796b0b08a01c7772c1f6f75a
Paul Melnikow <email@paulmelnikow.com>
2016-05-22 13:26:13 -0400

    Rewrite from apod-slack-lambda

--- README.md
+++ README.md
@@ -1,3 +1,41 @@
-node-aws-lambda-scheduled
+node-aws-lambda-scheduler
 =========================
 
+Minimalist utility to automatically [schedule AWS Lambda functions][schedule].
+
+Designed to be used in conjunction with ThoughtWorks Studios' minimalist library
+[node-aws-lambda][].
+
+[schedule]: http://docs.aws.amazon.com/AmazonCloudWatch/latest/DeveloperGuide/RunLambdaSchedule.html
+[node-aws-lambda]: https://github.com/ThoughtWorksStudios/node-aws-lambda
+
+
+Usage
+-----
+
+See [apod-slack-lambda][], which is an example which posts NASA's
+[Astronomy Picture of the Day][apod] to Slack.
+
+[apod]: http://apod.nasa.gov/apod/astropix.html
+[apod-slack-lambda]: https://github.com/paulmelnikow/apod-slack-lambda
+
+
+Contribute
+----------
+
+- Issue Tracker: https://github.com/paulmelnikow/node-aws-lambda-scheduler/issues
+- Source Code: https://github.com/paulmelnikow/node-aws-lambda-scheduler
+
+Pull requests welcome!
+
+
+Support
+-------
+
+If you are having issues, please let me know.
+
+
+License
+-------
+
+The project is licensed under the MIT license.
---
+++ index.js
@@ -0,0 +1,101 @@
+'use strict';
+
+const pify = require('pify');
+const AWS = require('aws-sdk');
+const _ = require('underscore');
+const c = require('rho-contracts');
+
+
+var cc = {};
+
+cc.awsRegion = require('rho-cc-aws-region');
+
+cc.lambdaSchedulerConfig = c.toContract({
+
+    region: cc.awsRegion,
+
+    accessKeyId: c.optional(c.string),
+    secretAccessKey: c.optional(c.string),
+    sessionToken: c.optional(c.string),
+
+    functionName: c.string
+        .doc('The name of the Lambda function being scheduled'),
+
+    ruleName: c.string
+        .doc("A name for the CloudWatch Events rule, e.g. '10am'"),
+
+    scheduleExpression: c.string
+        .doc('http://docs.aws.amazon.com/lambda/latest/dg/tutorial-scheduled-events-schedule-expressions.html'),
+
+}).rename('lambdaSchedulerConfig');
+
+
+class LambdaScheduler {
+
+    constructor (config) {
+        cc.lambdaSchedulerConfig.check(config);
+
+        const awsAttrs = _(config).pick(
+            'region', 'secretKeyId', 'secretAccessKey', 'secretToken');
+
+        this.cloudWatchEvents = new AWS.CloudWatchEvents(awsAttrs);
+        this.lambda = new AWS.Lambda(awsAttrs);
+
+        this.functionName = config.functionName;
+        this.ruleName = config.ruleName;
+        this.scheduleExpression = config.scheduleExpression;
+    }
+
+    // Update the CloudWatch event and return its ARN via promise.
+    updateEvent () {
+        const cloudWatchEvents = this.cloudWatchEvents;
+        const listRules = pify(cloudWatchEvents.listRules.bind(cloudWatchEvents));
+        const putRule = pify(cloudWatchEvents.putRule.bind(cloudWatchEvents));
+
+        const ruleAttrs = {
+            Name: this.ruleName,
+            ScheduleExpression: this.scheduleExpression,
+        };
+
+        return listRules({})
+            .then((rules) => {
+                const matching = _(rules.Rules).findWhere(ruleAttrs);
+
+                if (matching) {
+                    return matching.Arn;
+                } else {
+                    return putRule(ruleAttrs).then((data) => data.RuleArn);
+                }
+            });
+    }
+
+    _getFunctionArn () {
+        const getFunction = pify(this.lambda.getFunction.bind(this.lambda));
+
+        return getFunction({ FunctionName: this.functionName })
+            .then((functionData) => functionData.Configuration.FunctionArn);
+    }
+
+    updateEventTarget () {
+        const cloudWatchEvents = this.cloudWatchEvents;
+        const putTargets = pify(cloudWatchEvents.putTargets.bind(cloudWatchEvents));
+
+        this._getFunctionArn()
+            .then((functionArn) => {
+                const targetAttrs = {
+                    Rule: this.ruleName,
+                    Targets: [{ Id: '1', Arn: functionArn }],
+                };
+
+                return putTargets(targetAttrs);
+            });
+    }
+
+    schedule () {
+        return this.updateEvent()
+            .then(() => this.updateEventTarget());
+    }
+
+}
+
+exports.LambdaScheduler = LambdaScheduler;
